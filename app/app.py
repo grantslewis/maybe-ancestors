6015bf5353f1c4562eab46b6d63a9cc666d3ea2d
Grant Lewis <eagleflyer2500@gmail.com>
2023-09-16 13:57:41 -0600

    model swapped

--- app/app.py
+++ app/app.py
@@ -3,10 +3,11 @@ import torch
 import base64
 import io
 from PIL import Image
-from transformers import BlipProcessor, BlipForConditionalGeneration
-from diffusers import StableDiffusionXLControlNetPipeline, ControlNetModel, AutoencoderKL
+# from transformers import BlipProcessor, BlipForConditionalGeneration
+# from diffusers import DiffusionPipeline, StableDiffusionXLControlNetPipeline, ControlNetModel, AutoencoderKL
+from diffusers import StableDiffusionXLPipeline, AutoencoderKL, StableDiffusionXLImg2ImgPipeline
 # from generation import avatar_generation as avatar_generation
-import avatar_generation
+# import avatar_generation
 import random
 
 # Initialize the Flask application and configurations
@@ -14,32 +15,47 @@ app = Flask(__name__)
 
 caption_path = "Salesforce/blip-image-captioning-large"
 caption_text = ""
-control_net_path = "diffusers/controlnet-canny-sdxl-1.0"
-vae_path = "madebyollin/sdxl-vae-fp16-fix"
-diffuser_path = "stabilityai/stable-diffusion-xl-base-1.0"
-device = "cuda"
+# control_net_path = "diffusers/controlnet-canny-sdxl-1.0"
+VAE_PATH = "madebyollin/sdxl-vae-fp16-fix"
+DIFFUSER_PATH = "stabilityai/stable-diffusion-xl-base-1.0"
+DEVICE = "cuda"
 
-images_to_generate = 4
+
+
+
+IMAGE_COUNT = 4
+INITIAL_STEPS = 10
+HIGH_NOISE_FRAC = 0.7
+
+PRE_PROMPT = "8k, RAW photo, best quality, masterpiece, highly detailed, realistic style, photo-realistic, uhd, DSLR, soft lighting, film grain, high dynamic range, an avatar of a  " + prompt
+NEGATIVE_PROMPT = "soft line, lowres, text, sketch, bad hands, extra digit, fewer digits, cropped, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry, artist name, blurry, ugly, logo, pixelated, oversharpened, high contrast"
+
+
 
 def make_generators(count):
     seeds = [random.randint(0, 1000) for _ in range(count)]
-    generators = [torch.Generator(device=device).manual_seed(i) for i in seeds]
+    generators = [torch.Generator(device=DEVICE).manual_seed(i) for i in seeds]
     # for i in range(count):
     #     generators.append(random.randint(0, 100))
     return seeds, generators
 
-seeds, generators = make_generators(images_to_generate)
+seeds, generators = make_generators(IMAGE_COUNT)
 
 
 # generators = 
 
 # Initialize models outside routes
-cap_processor = BlipProcessor.from_pretrained(caption_path)
-cap_model = BlipForConditionalGeneration.from_pretrained(caption_path, torch_dtype=torch.float16).to(device)
-controlnet = ControlNetModel.from_pretrained(control_net_path, torch_dtype=torch.float16)
-vae = AutoencoderKL.from_pretrained(vae_path, torch_dtype=torch.float16)
-pipe = StableDiffusionXLControlNetPipeline.from_pretrained(diffuser_path, controlnet=controlnet, vae=vae, torch_dtype=torch.float16)
+# cap_processor = BlipProcessor.from_pretrained(caption_path)
+# cap_model = BlipForConditionalGeneration.from_pretrained(caption_path, torch_dtype=torch.float16).to(device)
+# controlnet = ControlNetModel.from_pretrained(control_net_path, torch_dtype=torch.float16)
+vae = AutoencoderKL.from_pretrained(VAE_PATH, torch_dtype=torch.float16)
+pipe = StableDiffusionXLPipeline.from_pretrained(DIFFUSER_PATH, vae=vae, torch_dtype=torch.float16, variant="fp16", use_safetensors=True)
+pipe = pipe.to(DEVICE)
+# pipe = StableDiffusionXLControlNetPipeline.from_pretrained(DIFFUSER_PATH, controlnet=controlnet, vae=vae, torch_dtype=torch.float16)
 pipe.enable_model_cpu_offload()
+
+refiner = StableDiffusionXLImg2ImgPipeline.from_pretrained("stabilityai/stable-diffusion-xl-refiner-1.0", torch_dtype=torch.float16)
+refiner = refiner.to(DEVICE)
 
 
 
@@ -51,35 +67,57 @@ def index():
 @app.route('/transform', methods=['POST'])
 def transform_image():
     try:
-        seeds, generators = make_generators(images_to_generate)
-        
-        
+        seeds, generators = make_generators(IMAGE_COUNT)
         data = request.json
-        image_data = base64.b64decode(data['imageBase64'])
-        image = Image.open(io.BytesIO(image_data))
-        controlnet_conditioning_scale = float(data.get('intensityLevel'))  
-        # Convert RGBA to RGB with a white background
-        if image.mode == 'RGBA':
-            background = Image.new("RGB", image.size, (255, 255, 255))
-            background.paste(image, mask=image.split()[3])  # 3 is the alpha channel
-            image = background  # Now 'image' is an RGB image
-        image = image.resize((1024, 1024))
-
         prompt = data['inputText']
-        negative_prompt = "soft line, lowres, text, sketch, extra digit, fewer digits, cropped, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry, artist name, blurry, ugly, logo, pixelated, oversharpened, high contrast"
-        print('PROMPT: ', prompt)
-        if len(prompt) == 0:
-            prompt = avatar_generation.caption_image(cap_processor, cap_model, image, text=caption_text, device=device)
-        print(prompt)
-        prompt = "8k, RAW photo, best quality, masterpiece, highly detailed, realistic style, photo-realistic, uhd, DSLR, soft lighting, film grain, high dynamic range, an avatar of a  " + prompt
-        result_image = avatar_generation.generate_avatar(pipe, prompt, controlnet_conditioning_scale, image, negative_prompt=negative_prompt)
-        result_image.save("result_image.jpg")  # You mentioned you don't want to save the new image
-
-        buffered = io.BytesIO()
-        result_image.save(buffered, format="PNG")
-        img_str = base64.b64encode(buffered.getvalue())
+        prompt
         
-        return jsonify({'result_image': img_str.decode('utf-8')})
+        images = pipe(prompt=prompt, num_inference_steps=INITIAL_STEPS, denoising_end=HIGH_NOISE_FRAC, num_images_per_prompt=IMAGE_COUNT, output_type="latent", negative_prompt=NEGATIVE_PROMPT).images
+        updated_images = []
+        ret_json = dict()
+        for i, image in enumerate(images):
+                # image.save("result_image.jpg")
+            image = refiner(prompt=prompt, num_inference_steps=INITIAL_STEPS, denoising_start=HIGH_NOISE_FRAC, image=image, negative_prompt=NEGATIVE_PROMPT).images[0]
+            updated_images.append(image)
+            image_name = f'result_image_{i + 1}'
+            image.save(f"{image_name}.jpg")
+            
+            buffered = io.BytesIO()
+            # result_image.save(buffered, format="PNG")
+            # img_str = base64.b64encode(buffered.getvalue())
+        
+        # return jsonify({'result_image': img_str.decode('utf-8')})
+            
+            
+            ret_json[f"{image_name}"] = base64.b64encode(buffered.getvalue()).decode('utf-8')
+        return jsonify(ret_json)
+        
+        
+        
+        
+        # # image_data = base64.b64decode(data['imageBase64'])
+        # # image = Image.open(io.BytesIO(image_data))
+        # controlnet_conditioning_scale = float(data.get('intensityLevel'))  
+        # # Convert RGBA to RGB with a white background
+        # if image.mode == 'RGBA':
+        #     background = Image.new("RGB", image.size, (255, 255, 255))
+        #     background.paste(image, mask=image.split()[3])  # 3 is the alpha channel
+        #     image = background  # Now 'image' is an RGB image
+        # image = image.resize((1024, 1024))
+
+        # prompt = data['inputText']
+        # print('PROMPT: ', prompt)
+        # if len(prompt) == 0:
+        #     prompt = avatar_generation.caption_image(cap_processor, cap_model, image, text=caption_text, device=DEFVICE)
+        # print(prompt)
+        # result_image = avatar_generation.generate_avatar(pipe, prompt, controlnet_conditioning_scale, image, negative_prompt=negative_prompt)
+        # result_image.save("result_image.jpg")  # You mentioned you don't want to save the new image
+
+        # buffered = io.BytesIO()
+        # result_image.save(buffered, format="PNG")
+        # img_str = base64.b64encode(buffered.getvalue())
+        
+        # return jsonify({'result_image': img_str.decode('utf-8')})
 
     except Exception as e:
         print(f'error: {str(e)}')
