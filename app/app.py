58c477c6d5f3420c74c26f3ae8096b27bfda0744
chasewes <chasemwesthoff@gmail.com>
2023-12-14 17:56:20 -0700

    new models

--- app/app.py
+++ app/app.py
@@ -15,123 +15,56 @@ from diffusers import StableDiffusionXLPipeline, AutoencoderKL, StableDiffusionX
 import random
 from model import ImageProcessor
 
+
 # Initialize the Flask application and configurations
 app = Flask(__name__)
 
-# caption_path = "Salesforce/blip-image-captioning-large"
-# caption_text = ""
-# # control_net_path = "diffusers/controlnet-canny-sdxl-1.0"
-# VAE_PATH = "madebyollin/sdxl-vae-fp16-fix"
-# DIFFUSER_PATH = "stabilityai/stable-diffusion-xl-base-1.0"
-# DEVICE = "cuda:0"
-
-
-
-
-
-
-# IMAGE_COUNT = 4
-# INITIAL_STEPS = 10
-# HIGH_NOISE_FRAC = 0.7
-
-# PROMPT_IMPROVMENT = "8k, RAW photo, best quality, masterpiece, highly detailed, realistic style, uhd, DSLR, soft lighting, film grain, high dynamic range," # photo-realistic, 
-# PROMPT_BASE = "A headshot of a"
-
-# NEGATIVE_PROMPT = "soft line, lowres, text, sketch, bad hands, extra digit, fewer digits, cropped, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry, artist name, blurry, ugly, logo, pixelated, oversharpened, high contrast, NSFW"
-
-
-
-# def make_generators(count):
-#     seeds = [random.randint(0, 1000) for _ in range(count)]
-#     generators = [torch.Generator(device=DEVICE).manual_seed(i) for i in seeds]
-#     # for i in range(count):
-#     #     generators.append(random.randint(0, 100))
-#     return seeds, generators
-
-# seeds, generators = make_generators(IMAGE_COUNT)
-
-
-# # generators = 
-
-# # Initialize models outside routes
-# # cap_processor = BlipProcessor.from_pretrained(caption_path)
-# # cap_model = BlipForConditionalGeneration.from_pretrained(caption_path, torch_dtype=torch.float16).to(device)
-# # controlnet = ControlNetModel.from_pretrained(control_net_path, torch_dtype=torch.float16)
-# vae = AutoencoderKL.from_pretrained(VAE_PATH, torch_dtype=torch.float16)
-# pipe = StableDiffusionXLPipeline.from_pretrained(DIFFUSER_PATH, vae=vae, torch_dtype=torch.float16, variant="fp16", use_safetensors=True)
-# pipe = pipe.to(DEVICE)
-# # pipe = StableDiffusionXLControlNetPipeline.from_pretrained(DIFFUSER_PATH, controlnet=controlnet, vae=vae, torch_dtype=torch.float16)
-# pipe.enable_model_cpu_offload()
-
-# refiner = StableDiffusionXLImg2ImgPipeline.from_pretrained("stabilityai/stable-diffusion-xl-refiner-1.0", torch_dtype=torch.float16)
-# refiner = refiner.to(DEVICE)
 
 model = ImageProcessor()
-
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
+
 @app.route('/transform', methods=['POST'])
 def transform_image():
     try:
-        seeds, generators = make_generators(IMAGE_COUNT)
         data = request.json
-        
-        print(request)
-        # data = request.get_json()
-        print({key: val for key, val in data.items() if key != "image"})
-        print('includes image' if 'image' in data else 'does not include image')
-    
+
+        # Extract and print form data
+        name = data.get('name')
+        culture = data.get('culture')
+        occupation = data.get('occupation')
+        date_or_description = data.get('dateOrDescription')
+        age = data.get('age')
+
+        print(f'Name: {name}, Culture: {culture}, Occupation: {occupation}, Year: {date_or_description}, Age: {age}')
+
         # Extract base64 image data
-        image_data = None
-        if 'image' in data:
-            image_data = data['image']
-            if "base64," in image_data:
-                image_data = image_data.split("base64,")[1]
-            # Decode the base64 string
-            image_bytes = base64.b64decode(image_data)
-
-            # Convert bytes data to a PIL Image
-            image = Image.open(BytesIO(image_bytes))
-
-            # You can now use this image object for your purposes, e.g., processing, saving, etc.
-            # Example: Saving the image
-            image.save("uploaded_image.png")
-
-        # Run the model
-        images = model.run(data, image)
+        image_data = data.get('image')
         
-        ## Old method (not including image input)
-        # images = pipe(prompt=prompt, num_inference_steps=INITIAL_STEPS, denoising_end=HIGH_NOISE_FRAC, num_images_per_prompt=IMAGE_COUNT, output_type="latent", negative_prompt=NEGATIVE_PROMPT).images
-        updated_images = []
-        ret_info = dict()
-        for i, image in enumerate(images):
-                # image.save("result_image.jpg")
-            image = refiner(prompt=prompt, num_inference_steps=INITIAL_STEPS, denoising_start=HIGH_NOISE_FRAC, image=image, negative_prompt=NEGATIVE_PROMPT).images[0]
-            updated_images.append(image)
-            image_name = f'result_image_{i + 1}'
-            image.save(f"{image_name}.jpg")
-            
+        # Processing the image data
+        image_data = image_data.split("base64,")[1] if "base64," in image_data else image_data
+        image_bytes = base64.b64decode(image_data)
+        image = Image.open(io.BytesIO(image_bytes))
+
+        result_images = model.run(data, image)
+
+        encoded_images = {}
+        for i, img in enumerate(result_images):
             buffered = io.BytesIO()
-            image.save(buffered, format="PNG")
-            img_str = base64.b64encode(buffered.getvalue()) 
-            
-            ret_info[f"{image_name}"] = img_str.decode('utf-8')
-        return jsonify({'result_images': ret_info, 'prompt': prompt_base})
+            img.save(buffered, format="JPEG")
+            img_str = base64.b64encode(buffered.getvalue()).decode('utf-8')
+            encoded_images[f'result_image_{i + 1}'] = f'data:image/jpeg;base64,{img_str}'
+
+        return jsonify({'result_images': encoded_images})
         
     except Exception as e:
         print(f'error: {str(e)}')
         return jsonify({'error': str(e)})
     
-@app.route('/modify', methods=['POST'])
-def modify_image():
-    data = request.json
-    num = data['num']
-    
-    
-
 if __name__ == '__main__':
     port = 5003
     app.run(debug=True, port=port)
